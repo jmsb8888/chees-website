96be35fb8d0f5f6ca84af564527a1553205fa207
Jose Salamanca <jsalamanca@automatiza.co>
2025-09-27 23:42:48 -0500

    feat: port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
